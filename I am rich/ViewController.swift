4ed6c746ae068f922fb9c213899d786fbf78843f
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2021-05-24 23:49:24 -0600

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Kien Nguyen on 2021-05-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
